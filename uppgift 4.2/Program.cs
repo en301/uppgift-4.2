d9dc996ee19450f724d51a01e401e435e7c65242
en301 <emilsjostrand1000@gmail.com>
2023-10-18 10:22:19 +0200

    Add project files.

---
+++ uppgift 4.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace uppgift_4_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int tal2 = 0;
+            while(true)
+            {
+                Console.WriteLine("Vill du skriva in ett heltal eller avsluta programmet?");
+                string svar = Console.ReadLine().ToLower();
+                if (svar == "avsluta")
+                {
+                    break;
+                }
+                Console.WriteLine("Skriv in ett heltal");
+                int tal = int.Parse(Console.ReadLine());
+                if ( tal > tal2)
+                {
+                    tal2 = tal;
+                }
+            }
+            Console.WriteLine("Det största talet du skrev var " + tal2);
+            Console.ReadKey();
+        }
+    }
+}
